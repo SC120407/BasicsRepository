32c8d5f2f4a9e1e52dcb43344c91d6bbffa21d2a
SC120407 <163951468+SC120407@users.noreply.github.com>
2024-03-22 18:19:54 -0700

    Update #1 DiceRollSim.py
    Deleted math import library. Used f" string when working with variables

--- DiceRollSim.py
+++ DiceRollSim.py
@@ -1,5 +1,4 @@
 #Diceroll
-import math
 import random
 
 lower_bound = int(input("Lower Bound: "))
@@ -8,7 +7,7 @@ upper_bound = int(input("Upper Bound: "))
 if lower_bound < upper_bound:
     num_1 = random.randint(lower_bound, upper_bound)
     num_2 = random.randint(lower_bound, upper_bound)
-    print(str(num_1) + ", " + str(num_2))
+    print(f"{num_1}, {num_2}")
     print(num_1 + num_2)
 else:
     print("Error in Bounds")
