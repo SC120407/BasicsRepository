9aac0dd6964ce331b31b530f8d7f3a6cfa750342
SC120407 <163951468+SC120407@users.noreply.github.com>
2024-03-22 18:23:58 -0700

    Update Calculatorproject.py

--- Calculatorproject.py
+++ Calculatorproject.py
@@ -1,52 +1,48 @@
 #Calculator project
-def calc():
-    #First number input
-    #Second number input
+#First number input
+#Second number input
 
-    print("addition: +")
-    print("subtraction: -")
-    print("multiplication: *")
-    print("division: /")
-    print("exponent: ^")
-    print("floor dision: //")
-    print("modulo: mod")
-    operation = (input("Operation: "))
+print("addition: +")
+print("subtraction: -")
+print("multiplication: *")
+print("division: /")
+print("exponent: ^")
+print("floor dision: //")
+print("modulo: mod")
+operation = (input("Operation: "))
 
-    if operation == "+":
-        num_1 = float(input("First Number: "))
-        num_2 = float(input("Second Number: "))
-        print(num_1 + num_2)
-    elif operation == "-":
-        num_1 = float(input("First Number: "))
-        num_2 = float(input("Second Number: "))
-        print(num_1 - num_2)
-    elif operation == "*":
-        num_1 = float(input("First Number: "))
-        num_2 = float(input("Second Number: "))
-        print(num_1 * num_2)
-    elif operation == "/":
-        num_1 = float(input("First Number: "))
-        num_2 = float(input("Second Number: "))
-        print(num_1 / num_2)
-    elif operation == "^":
-        num_1 = float(input("First Number: "))
-        num_2 = float(input("Second Number: "))
-        print(num_1 ** num_2)
-    elif operation == "//":
-        num_1 = float(input("First Number: "))
-        num_2 = float(input("Second Number: "))
-        print(num_1 // num_2)
-    elif operation == "mod":
-        num_1 = float(input("First Number: "))
-        num_2 = float(input("Second Number: "))
-        print(num_1 % num_2)
-    else:
-        print("Invalid operation")
-
-calc()
-Choice = input.("Enter Your Expression:")
-print(eval(Choice))
+if operation == "+":
+    num_1 = float(input("First Number: "))
+    num_2 = float(input("Second Number: "))
+    print(num_1 + num_2)
+elif operation == "-":
+    num_1 = float(input("First Number: "))
+    num_2 = float(input("Second Number: "))
+    print(num_1 - num_2)
+elif operation == "*":
+    num_1 = float(input("First Number: "))
+    num_2 = float(input("Second Number: "))
+    print(num_1 * num_2)
+elif operation == "/":
+    num_1 = float(input("First Number: "))
+    num_2 = float(input("Second Number: "))
+    print(num_1 / num_2)
+elif operation == "^":
+    num_1 = float(input("First Number: "))
+    num_2 = float(input("Second Number: "))
+    print(num_1 ** num_2)
+elif operation == "//":
+    num_1 = float(input("First Number: "))
+    num_2 = float(input("Second Number: "))
+    print(num_1 // num_2)
+elif operation == "mod":
+    num_1 = float(input("First Number: "))
+    num_2 = float(input("Second Number: "))
     print(num_1 % num_2)
 else:
     print("Invalid operation")
+Choice = input.("Enter Your Expression:")
+print(eval(Choice))
+    print(num_1 % num_2)
+
 
